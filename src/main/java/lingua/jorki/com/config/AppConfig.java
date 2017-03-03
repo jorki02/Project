248598905b3637beac762be1f10e3d2fb7202190
jorki <jorki02@gmail.com>
2017-03-03 14:51:16 +0300

    Начало

---
+++ src/main/java/lingua/jorki/com/config/AppConfig.java
@@ -0,0 +1,62 @@
+package erpsystem.config;
+
+
+import erpsystem.model.CreateTable;
+import erpsystem.model.Hello;
+import erpsystem.model.TableManagers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+
+@Configuration
+@PropertySource(value = {"classpath:util.properties"})
+@PropertySource(value = {"classpath:auth.properties"})
+public class AppConfig {
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        JdbcDaoImpl jdbcImpl = new JdbcDaoImpl();
+        jdbcImpl.setDataSource(dataSource());
+        jdbcImpl.setUsersByUsernameQuery(environment.getRequiredProperty("usersByQuery"));
+        jdbcImpl.setAuthoritiesByUsernameQuery(environment.getRequiredProperty("rolesByQuery"));
+        return jdbcImpl;
+    }
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.postgresql.driverClass"));
+        dataSource.setUrl(environment.getProperty("jdbc.postgresql.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.postgresql.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.postgresql.password"));
+        return dataSource;
+    }
+    @Bean
+    public Hello hello(){
+        return new Hello("hello message");
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public CreateTable createTable(){
+        return new CreateTable(jdbcTemplate());
+    }
+
+    @Bean
+    public TableManagers tableManagers(){return new TableManagers(jdbcTemplate());}
+
+}
